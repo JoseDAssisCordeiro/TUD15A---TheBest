41cc75373b4d500eb2dcfe616f8ed74febaff5af
Xavi95 <javier.parraga.delacruz@alumnos.upm.es>
2017-03-13 22:02:05 +0100

    Update Platform.java

--- src/Game/Platform.java
+++ src/Game/Platform.java
@@ -126,8 +126,16 @@ public class Platform {
 		}
 	}
 	
-	public void checkGameOver(){
-		
+	public boolean checkGameOver(){
+		boolean someIqual=false;
+		for(int i=0;i<4;i++)
+			for(int j=0;i>4;i++){
+			if(compare_Squares(_squares[j][i], _squares[j][i + 1]))
+					someIqual=true;	
+			if(_squares[j][i].get_points()==2048)
+				return true;
+			}
+		return !someIqual;
 	}
 	
 	
