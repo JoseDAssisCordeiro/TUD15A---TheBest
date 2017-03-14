7079ad8e81e94ad520c8d99aea43c53d0025ae21
Pino Surace <pino.surace@student.tut.fi>
2017-03-14 13:03:28 +0100

    update

--- src/Game/Platform.java
+++ src/Game/Platform.java
@@ -69,6 +69,21 @@ public class Platform {
 				}
 			}
 		}
+		
+		
+		if (checkGameOver() == false){
+			boolean i =  true;
+			while (i){
+				Random random = new Random();
+				int j = random.nextInt(4);
+				int k = random.nextInt(4);
+				if (_squares[j][k].get_active() == false ){
+					_squares[j][k].set_active(true);
+					i = false;
+				}			
+			}
+		}
+		
 	}
 	
 	public void moveLeft(){
@@ -91,6 +106,18 @@ public class Platform {
 			}
 		}
 		
+		if (checkGameOver() == false){
+			boolean i =  true;
+			while (i){
+				Random random = new Random();
+				int j = random.nextInt(4);
+				int k = random.nextInt(4);
+				if (_squares[j][k].get_active() == false ){
+					_squares[j][k].set_active(true);
+					i = false;
+				}			
+			}
+		}
 	}
 	
 	public void moveUp(){
@@ -110,6 +137,18 @@ public class Platform {
 					combineSquares(_squares[j][i], _squares[j][i + 1]);
 					
 				}
+			}
+		}
+		if (checkGameOver() == false){
+			boolean i =  true;
+			while (i){
+				Random random = new Random();
+				int j = random.nextInt(4);
+				int k = random.nextInt(4);
+				if (_squares[j][k].get_active() == false ){
+					_squares[j][k].set_active(true);
+					i = false;
+				}			
 			}
 		}
 	}
@@ -149,6 +188,18 @@ public class Platform {
 					combineSquares(_squares[j][i - 1], _squares[j][i]);
 					
 				}
+			}
+		}
+		if (checkGameOver() == false){
+			boolean i =  true;
+			while (i){
+				Random random = new Random();
+				int j = random.nextInt(4);
+				int k = random.nextInt(4);
+				if (_squares[j][k].get_active() == false ){
+					_squares[j][k].set_active(true);
+					i = false;
+				}			
 			}
 		}
 	}
