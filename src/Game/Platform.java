4dc02f90cfb559a934d05035f2997019028ba184
José d'Assis <josemc95@hotmail.com>
2017-03-15 16:13:57 +0100

    State pattern part 1

--- src/Game/Platform.java
+++ src/Game/Platform.java
@@ -223,7 +223,7 @@ public class Platform {
 			return false;
 		
 		for(int i = 0; i < 3; i++)
-			for(int j = 0; i < 3; i++){
+			for(int j = 0; j < 3; j++){
 				if(compareSquares(_squares[j][i], _squares[j][i + 1]))
 					return false;
 
---
+++ src/Game/State.java
@@ -0,0 +1,13 @@
+package Game;
+
+public interface State {
+	
+	public void PressUp();
+	public void PressDown();
+	public void PressLeft();
+	public void PressRight();
+	public void PressP();
+	public void PressR();
+	public void PressEnter();	
+	
+}
